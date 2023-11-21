f592009a79646ef0e124aa6650161ddb0af70707
Mr Stone <pierre.evens16@gmail.com>
2023-11-20 23:47:56 -0500

    Implement URIMapper

--- src/mappers/URIMapper.mjs
+++ src/mappers/URIMapper.mjs
@@ -1,3 +1,13 @@
 export class URIMapper {
-  map (value) {}
+
+  async map (request, options = {}) {
+    const url = new URL(request.url, `http://${options.url}`)
+    
+    return {
+      url,
+      method: 'GET',
+      queryString: url.search,
+      protocol: url.protocol.replace(':', ''),
+    }
+  }
 }
