77e1901d8edfc9f170b4cbc026fa380259cbcb30
Mr Stone <pierre.evens16@gmail.com>
2024-05-12 16:53:53 -0400

    Implement options exporter

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,4 @@
+import copy from 'rollup-plugin-copy'
 import babel from '@rollup/plugin-babel'
 import multi from '@rollup/plugin-multi-entry'
 import commonjs from '@rollup/plugin-commonjs'
@@ -20,6 +21,11 @@ export default Object.entries(inputs).map(([name, input]) => ({
       exportConditions: ['node', 'import', 'require', 'default']
     }),
     babel({ babelHelpers: 'bundled' }),
-    commonjs()
+    commonjs(),
+    copy({
+      targets: [
+        { src: 'src/config/options.mjs', dest: 'dist' }
+      ]
+    })
   ]
 }))
